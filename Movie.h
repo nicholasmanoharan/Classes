d7159c7154d8b382c8b09342334c9170fa05e608
nicholasmanoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2023-11-09 21:27:46 -0800

    Update and rename Movie to Movie.h

--- Movie.h
+++ Movie.h
@@ -2,16 +2,17 @@
 #include "Media.h"
 
 class Movie : public Media {
-
-public: 
-    movie (const std::string& title, int year, const std::stirng& director, int duration);
+public:
+    Movie(const std::string& title, int year, const std::string& director, int duration, int rating);
     void displayInfo() const override;
     bool search(const std::string& term) const override;
+
+    const std::string& getDirector() const;
+    int getDuration() const;
     int getRating() const;
 
 private:
     std::string director;
     int duration;
     int rating;
-
 };
